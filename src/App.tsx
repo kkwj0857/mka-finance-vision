fbfe0ea38f161ad2473ecdf148cb8b3f72f6e7f4
Imran <imran.mahmoodur@gmail.com>
2026-04-28 22:51:23 +0100

    remove vercel config

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>MKA Finance Vision</h1>
+      <p>Site is working</p>
+    </div>
+  );
+}
